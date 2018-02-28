3e82034fa24170d38037e1d01082e08b16b7e89b
HOkkerman <H.E.Okkerman@gmail.com>
2018-02-28 14:57:50 +0100

    Improved readability

--- matlab/FM_demod.m
+++ matlab/FM_demod.m
@@ -1,8 +1,5 @@
 function [z_out, z_B2, z_N2, z_dis, y_N1, y_B1]=FM_demod(x, B1, N1, B2, N2, fs)
-% subplot(4,1,1)
-% simpleSA(x, 2^14,fs/1000);
-
-%Lowpass filter 1
+%Lowpass filter 1------------------------------------------------------
 %Butter
 order=6;
 [b_1,a_1] = butter(order,B1/(fs/2));
@@ -13,25 +10,19 @@ y_B1=filter(b_1,a_1,x);
 %b_1=fir1(order,B1/(fs/2));
 %y_B1=filter(b_1,1,x);
 
-% subplot(4,1,2)
-% simpleSA(y_B1, 2^14,fs/1000);
 
-%Downsampling 1
+%Downsampling 1---------------------------------------------------------
 %decimate(x,r) includes LPF which would be redundant
 %so downsample(x,n) is used instead
 y_N1=downsample(y_B1,N1);
 fs_1=fs/N1;
 
-% subplot(4,1,3)
-% simpleSA(y_N1, 2^14,fs_1/1000);
 
-%Discriminator
+%Discriminator----------------------------------------------------------
 z_dis=discrim(y_N1);
 
-% subplot(4,1,4)
-% simpleSA(z_dis, 2^14,fs_1/1000);
 
-%Lowpass filter 2
+%Lowpass filter 2-------------------------------------------------------
 %Butter
 order=6;
 [b_2,a_2] = butter(order,B2/(fs_1/2));
@@ -44,20 +35,23 @@ z_B2=filter(b_2,a_2,z_dis);
 
 
 
-%Downsampling 2
+%Downsampling 2---------------------------------------------------------
 z_N2=downsample(z_B2,N2);
 fs_2=fs_1/N2;
 
-%simpleSA(z_B2, 2^14,fs_1/1000);
 %soundsc(z_N2, fs_2);
 
-%De-emphasis filter
-%Emphasis time constant Europe
+%De-emphasis filter-----------------------------------------------------
+%Emphasis time constant (Europe: 50us, US: 75us)
 tau=50*10^-6;
 %Feedback coefficient
 a1=exp(-1/(tau*fs_2));
 %Filters
 a=[1,-a1];
 b=[1-a1];
+%Final audio output----------------------------------------------------
 z_out=filter(b,a,z_N2);
+
+
+%soundsc(z_N2, fs_2);
 
