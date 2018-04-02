b0a2db7720c13db8291fb3d4ddcc7aa895c21893
hokkerman <h.e.okkerman@gmail.com>
2018-04-02 20:50:37 +0200

    Pushed backup of my files, will try to merge manually

---
+++ SDR/Ratecontrol/Hans_backup/runratecontrol.m
@@ -0,0 +1,59 @@
+%This file is used to run ratecontrol.m with specific input settings.
+%Ratecontrol.m is the same file as TransmitRateControlExample.m, except its
+%contents are turned into a large function. Some settings that control the
+%wireless channel can be set as inputs which eases simulating many
+%different channel types. 
+%
+%
+
+%TODO: Put the rate control algorithm in its own separate function to ease
+%editing and changing algorithms
+
+%Bandwidth of channel. Can be CBW20, CBW40, CBW80 or CBW160
+bandwidth_i=['CBW20 '; 'CBW40 '; 'CBW80 '; 'CBW160'];
+bandwidth=cellstr(bandwidth_i);
+
+%Delay profile model. Can be Model-A through Model-F. Higher is more
+%reflections and larger delays
+delay_profile_i=['Model-A'; 'Model-B'; 'Model-C'; 'Model-D'; 'Model-E'; 'Model-F' ];
+delay_profile=cellstr(delay_profile_i);
+%Distance is the distance between Tx and Rx. Determines if there is a LOS
+%condition based on the chosen delay profile.
+distance=10;
+npackets=100;
+
+historysize=6;
+% weights=ones(1,historysize);
+weights=[1,1,2,2,3,5];
+
+
+
+
+avg1=zeros(length(bandwidth), length(delay_profile));
+avg2=zeros(length(bandwidth), length(delay_profile));
+avg3=zeros(length(bandwidth), length(delay_profile));
+
+%mov1=zeros(length(bandwidth), length(delay_profile));
+%mov2=zeros(length(bandwidth), length(delay_profile));
+%mov3=zeros(length(bandwidth), length(delay_profile));
+
+
+for bw=2:length(bandwidth)
+    for dp=1:length(delay_profile)
+        bandwidth_in=char(bandwidth(bw))
+        delay_profile_in=char(delay_profile(dp))
+        distance_in=distance;
+       
+       
+        [avg1(bw, dp), mov1] = ratecontrol_old(npackets, bandwidth_in, delay_profile_in, distance_in);
+        [avg2(bw, dp), mov2] = ratecontrol(npackets, bandwidth_in, delay_profile_in, distance_in, historysize);
+        [avg3(bw, dp), mov3] = ratecontrol_weighted(npackets, bandwidth_in, delay_profile_in, distance_in, weights);    
+    end
+
+
+end
+        
+
+% mov = ratecontrol(bandwidth_in, delay_profile_in, distance_in);
+% avg_datarate=mean(mov);
+
