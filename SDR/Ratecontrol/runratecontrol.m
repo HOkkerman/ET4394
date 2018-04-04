3fd8ec0d76bd4141981d871bb7cb240fa542bf34
hokkerman <h.e.okkerman@gmail.com>
2018-04-04 20:35:57 +0200

    Final improvements to report and added more comments to runratecontrol

--- SDR/Ratecontrol/runratecontrol.m
+++ SDR/Ratecontrol/runratecontrol.m
@@ -1,12 +1,13 @@
-%This file is used to run ratecontrol.m with specific input settings.
-%Ratecontrol.m is the same file as TransmitRateControlExample.m, except its
-%contents are turned into a large function. Some settings that control the
+%This file is used to run ratecontrol_xxxxx.m with specific input settings.
+%Ratecontrol_xxxx.m is the same file as TransmitRateControlExample.m,
+%except its contents are turned into a large function and the actual rate
+%control algorithm varies based on the name. Some settings that control the
 %wireless channel can be set as inputs which eases simulating many
-%different channel types. 
+%different channel types.
 %
 %
 
-%Bandwidth of channel. Can be CBW20, CBW40, CBW80 or CBW160
+%Bandwidth of channel. Can be 20, 40, 80 or 160MHz
 bandwidth_i=['CBW20 '; 'CBW40 '; 'CBW80 '; 'CBW160'];
 bandwidth=cellstr(bandwidth_i);
 
@@ -14,18 +15,20 @@ bandwidth=cellstr(bandwidth_i);
 %reflections and larger delays
 delay_profile_i=['Model-A'; 'Model-B'; 'Model-C'; 'Model-D'; 'Model-E'; 'Model-F' ];
 delay_profile=cellstr(delay_profile_i);
-%Distance is the distance between Tx and Rx. Determines if there is a LOS
-%condition based on the chosen delay profile.
-%distance=10;
 
-npackets=50;
+%Amount of packets to simulate
+npackets=500;
+
+%History size for averaging algorithm
 historysize=6;
-% weights=ones(1,historysize);
+
+%Weights for weighted averaging algorithm
 weights=[1,1,2,2,3,5];
 
+%Breakpoint distances for corresponding delay models
 breakpoint_distance = [5; 5; 5; 10; 20; 30];
 
-%Initialize arrays for results
+%Initialize arrays for results just for fancy reasons
 %Average throughput
 avg_original=zeros(length(bandwidth), length(delay_profile), 3);
 avg_average=zeros(length(bandwidth), length(delay_profile), 3);
@@ -37,12 +40,14 @@ PER_average=zeros(length(bandwidth), length(delay_profile), 3);
 PER_weighted=zeros(length(bandwidth), length(delay_profile), 3);
 PER_bandit=zeros(length(bandwidth), length(delay_profile), 3);
 
-% supress warnings
+% supress annoying warnings
 warning('off', 'wlan:shared:PSDULengthInvalidMCSCombination');
 warning('off', 'wlan:helperSampleRate:Deprecation');
 
+%Log time spent simulating between each result
 datetime('now')
 
+%Log total time
 tic
 for bw=1:4
     for dp=1:6
@@ -56,7 +61,7 @@ for bw=1:4
             %times 0.5, 1 and 1.5
             distance_in=d*0.5*breakpoint_distance(dp)
 
-            %Run all 4 models and store results in fuckhueg arrays
+            %Run all 4 models and store results in arrays. Also print times
             [avg_original(bw, dp, d), PER_original(bw, dp, d)] = ratecontrol_old(npackets, bandwidth_in, delay_profile_in, distance_in);
             datetime('now')
             [avg_average(bw, dp, d), PER_average(bw, dp, d)] = ratecontrol(npackets, bandwidth_in, delay_profile_in, distance_in, historysize);
